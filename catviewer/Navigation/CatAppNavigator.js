9d0088f04adde11550a14a5c8e5ddcca7d1478da
kevin stevanus <kevinstevanus777@gmail.com>
2020-08-18 16:46:59 +0700

    create home screen page

--- catviewer/Navigation/CatAppNavigator.js
+++ catviewer/Navigation/CatAppNavigator.js
@@ -1,13 +1,34 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import CatPage from "../Screens/ListCatPage";
 
 const Stack = createStackNavigator();
 
-function myStack() {
+function HomeScreen() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home"></Stack.Screen>
-      <Stack.Screen name="Profile"></Stack.Screen>
-    </Stack.Navigator>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+function myStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home"></Stack.Screen>
+        <Stack.Screen name="Details"></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default myStack;
--- catviewer/Screens/ListCatPage.js
+++ catviewer/Screens/ListCatPage.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
 const CatPage = () => {
   return (
     <View>
-      <Text>this is listcat page</Text>
+      <Text>this is list cat page</Text>
     </View>
   );
 };

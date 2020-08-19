56214332cbecad9ae2c074efe8fe3053bd2e375e
kevin stevanus <kevinstevanus777@gmail.com>
2020-08-19 11:24:52 +0700

    try fetch API

--- catviewer/Screens/ListCatPage.js
+++ catviewer/Screens/ListCatPage.js
@@ -1,10 +1,30 @@
 import * as React from "react";
-import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
-
+import { StyleSheet, Text, View, Image } from "react-native";
+const fetchDogs = () => {
+  return fetch("https://reactnative.dev/movies.json")
+    .then((response) => response.json())
+    .then((json) => {
+      return json.movies;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 const CatPage = () => {
+  console.log(fetchDogs());
   return (
     <View style={styles.screen}>
-      <Text>this is list cat page, yesh</Text>
+      <View style={styles.test}>
+        <Text>test</Text>
+        <Image
+          source={{
+            uri:
+              "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+        <Text>test</Text>
+      </View>
     </View>
   );
 };
@@ -13,6 +33,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  test: {
+    alignItems: "center",
+    flex: 1,
   },
 });
 
--- catviewer/Screens/SignatureScreen.js
+++ catviewer/Screens/SignatureScreen.js
@@ -1,14 +1,16 @@
 import * as React from "react";
+import SignatureCapture from "react-native-signature-capture";
 
 import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
 
 const SignaturePage = () => {
   return (
     <View style={styles.screen}>
-      <Text>signature page</Text>
+      <Text>test</Text>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   screen: {
     flex: 1,

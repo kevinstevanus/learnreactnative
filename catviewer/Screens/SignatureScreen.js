77305fff99c9bb54587bcd9283ee25237f28beb3
kevin stevanus <kevinstevanus777@gmail.com>
2020-08-19 13:49:23 +0700

    before i unlink

--- catviewer/Screens/SignatureScreen.js
+++ catviewer/Screens/SignatureScreen.js
@@ -3,13 +3,24 @@ import SignatureCapture from "react-native-signature-capture";
 
 import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
 
-const SignaturePage = () => {
-  return (
-    <View style={styles.screen}>
-      <Text>test</Text>
-    </View>
-  );
-};
+// const SignaturePage = () => {
+//   return (
+//     <View style={styles.screen}>
+//       <Text>test</Text>
+//     </View>
+//   );
+// };
+
+class SignaturePage extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>test</Text>
+        <SignatureCapture></SignatureCapture>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   screen: {
